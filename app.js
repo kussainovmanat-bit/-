aa7a290cfeffbed823f0be04dce11e2b8a718aae
kussainovmanat-bit <kussainov.manat@gmail.com>
2026-02-09 22:55:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const timeInput = document.getElementById('time');
+
+if ('Notification' in window) {
+  Notification.requestPermission();
+}
 const dateInput = document.getElementById('date');
 const taskInput = document.getElementById('task');
 const list = document.getElementById('list');
@@ -14,12 +19,19 @@ function storageKey() {
 function loadTasks() {
   list.innerHTML = '';
   const tasks = JSON.parse(localStorage.getItem(storageKey())) || [];
+if (tasks.length === 0) {
+  list.innerHTML = '<li>📭 Дел на эту дату нет</li>';
+  return;
+}
 
   tasks.forEach((task, index) => {
     const li = document.createElement('li');
 
     const span = document.createElement('span');
-    span.textContent = task.text;
+    span.textContent = task.time
+  ? `${task.time} — ${task.text}`
+  : task.text;
+
     if (task.done) span.classList.add('done');
     span.onclick = () => toggleTask(index);
 
@@ -41,12 +53,24 @@ function addTask() {
   if (!taskInput.value) return;
 
   const tasks = JSON.parse(localStorage.getItem(storageKey())) || [];
-  tasks.push({ text: taskInput.value, done: false });
+
+  tasks.push({
+    text: taskInput.value,
+    time: timeInput.value,
+    done: false
+  });
+
   localStorage.setItem(storageKey(), JSON.stringify(tasks));
 
+  if (timeInput.value) {
+    scheduleNotification(taskInput.value, timeInput.value);
+  }
+
   taskInput.value = '';
+  timeInput.value = '';
   loadTasks();
 }
+
 
 function toggleTask(index) {
   const tasks = JSON.parse(localStorage.getItem(storageKey()));
@@ -60,4 +84,21 @@ function deleteTask(index) {
   tasks.splice(index, 1);
   localStorage.setItem(storageKey(), JSON.stringify(tasks));
   loadTasks();
+}
+
+function scheduleNotification(text, time) {
+  const [hours, minutes] = time.split(':');
+  const now = new Date();
+  const notifyTime = new Date(dateInput.value);
+
+  notifyTime.setHours(hours, minutes, 0);
+
+  const delay = notifyTime - now;
+  if (delay <= 0) return;
+
+  setTimeout(() => {
+    new Notification('Напоминание', {
+      body: text
+    });
+  }, delay);
 }
